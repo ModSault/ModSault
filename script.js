9609d15500ce3f063a7a35819c5440565a2c7464
ModSault <modsault@gmail.com>
2025-04-18 19:27:41 -0400

    updated base scripts and styling
    Local storage support, a way to make multiple selectors for game versions, nav bar css, and functions to automatically make headers and such consistent between pages.

--- script.js
+++ script.js
@@ -2,25 +2,48 @@ var advancedMode = false;
 var toolTipTimer = null;
 var tooltipDiv = null;
 var GameVersion = 0; // 0 = USA, 1 = Japan, 2 = PAL
+var allGameModeSelectors = [];
 
-// ---------------------------- General Changeing Game Versions --------------------------
+// ---------------------------- Load Information from Local Storage  --------------------------
+
+function loadInfo() {
+    // Game Version
+    GameVersion = Math.max(0, Math.min(localStorage.getItem("GameMode"), 2));
+    if (isNaN(GameVersion) || (typeof GameVersion) != "number") {
+        localStorage.setItem("GameMode", 0);
+        GameVersion = 0;
+    }
+    
+    // Advanced Mode
+    advancedMode = localStorage.getItem("AdvancedMode").toLowerCase() === 'true';
+    localStorage.setItem("AdvancedMode", advancedMode);
+}
+loadInfo();
+
+// ---------------------------- General Changing Game Versions --------------------------
 
 function changeGameVersion(object) {
     GameVersion = object.value;
+    localStorage.setItem("GameMode", object.value);
+
+    // update all game mode selectors to the new value
+    for (i = 0; i < allGameModeSelectors.length; i++) {
+        allGameModeSelectors[i].value = GameVersion;
+    }
 }
 
 // ---------------------------- Advanced Mode --------------------------
 
 function toggleAdvanced() {
     button = document.getElementById("AdvancedModeButton");
-    if (advancedMode == true) {
+    if (advancedMode === true) {
         button.style.backgroundColor = "var(--red-button)";
     } else {
         button.style.backgroundColor = "var(--green-button)";
     }
     advancedMode = !advancedMode
+    localStorage.setItem("AdvancedMode", advancedMode);
     document.getElementById("AdvancedModeCheckMark").checked = advancedMode;
-    // ShowDetailsOfCurrentlySelectedSpawn(document.getElementById(currentSelected));
 }
 
 // ---------------------------- That one tooltip logic  --------------------------
@@ -44,4 +67,139 @@ function hoverToolTipEnd() {
         tooltipDiv.classList.remove('fade');
         tooltipDiv.remove();
     }
+}
+
+// ----------------------------- Making Content between pages consistent -------------------------
+
+function makeNavBar(pathToRoot) {
+    // JSON with Navbar contents. Makes it easier to update
+    AllElements = [
+        {
+            "text": "Home",
+            "link": pathToRoot + "/"
+        },
+        {
+            "text": "ID Documentation \u25BC",
+            "link": null,
+            "dropdownElements": [
+                {
+                    "text": "Projectile IDs",
+                    "link": pathToRoot + "/ProjectileID/Legacy/"
+                }
+            ]
+        }
+    ]
+
+    // <div id="NavBar">
+    let navBar = document.createElement("div");
+    navBar.id = "NavBar";
+
+    for (i = 0; i < AllElements.length; i++) {
+        if (AllElements[i]["link"] != null) { // for non-dropdown links
+            // <a href="./">Text</a>
+            let curLink = document.createElement("a");
+            curLink.href = AllElements[i]["link"];
+            curLink.text = AllElements[i]["text"];
+            navBar.appendChild(curLink);
+        } else {
+            /*<div class="NavBar_dropdown">
+                <p> Section Header </p>
+                <div>
+                    <a href="#">Link to Page</a>
+                    ...
+                </div>
+              </div>*/
+            let curDiv = document.createElement("div");
+            curDiv.classList.add("NavBar_dropdown");
+            let curHeader = document.createElement("p");
+            curHeader.textContent = AllElements[i]["text"];
+            curDiv.appendChild(curHeader);
+            let divContainer = document.createElement("div");
+            for (j = 0; j < AllElements[i]["dropdownElements"].length; j++) {
+                let curLink = document.createElement("a");
+                curLink.href = AllElements[i]["dropdownElements"][j]["link"];
+                curLink.text = AllElements[i]["dropdownElements"][j]["text"];
+                divContainer.appendChild(curLink);
+            }
+            curDiv.appendChild(divContainer);
+            navBar.appendChild(curDiv);
+        }
+    }
+
+    // add navbar to page
+    let scriptTag = document.currentScript;
+    scriptTag.parentNode.replaceChild(navBar, scriptTag);
+}
+
+
+function makeGameModeSelector(funcToCall) {
+    // <select name="GameVersion" id="GameVersionSelector" onChange="changeGameVersion(this); funcToCall();">
+    let topLevel = document.createElement("select");
+    topLevel.name = "GameVersion";
+    topLevel.className = "GameVersionSelector";
+    if (funcToCall != null) {
+        topLevel.onchange = function () { changeGameVersion(this); funcToCall(); };
+    } else {
+        topLevel.onchange = function () { changeGameVersion(this); };
+    }
+
+    // -<option value=all_values[i]> all_text[i] </option>
+    let all_values = [0, 1, 2];
+    let all_text = ["NTSC-U (USA)", "NTSC-J (Japan)", "PAL (Europe)"];
+    for (i = 0; i < all_values.length; i++) {
+        let curOption = document.createElement("option");
+        curOption.value = all_values[i];
+        curOption.textContent = all_text[i];
+        topLevel.appendChild(curOption);
+    }
+
+    topLevel.value = GameVersion; // set the default value to the current game version
+    allGameModeSelectors.push(topLevel); // add to list of all game mode selectors for updating all later
+
+    // add gamemode selector to page
+    let scriptTag = document.currentScript;
+    scriptTag.parentNode.replaceChild(topLevel, scriptTag);
+}
+function makeHeader(title, funcToCall = null) {
+    // <div class = "flexContainer">
+    let topLevel = document.createElement("div");
+    topLevel.classList.add("flexContainer");
+
+    // -<h1 id = "Title"> title </h1>
+    let header = document.createElement("h1");
+    header.id = "Title";
+    header.textContent = title;
+    topLevel.appendChild(header);
+
+    // -<div id="TopRightOfScreen">
+    let topRightDiv = document.createElement("div");
+    topRightDiv.id = "TopRightOfScreen";
+    topLevel.appendChild(topRightDiv);
+
+    // --<script>makeGameModeSelector(funcToCall);</script>
+    let gameModeSelector = document.createElement("script");
+    gameModeSelector.textContent = "makeGameModeSelector("+funcToCall+");";
+    topRightDiv.appendChild(gameModeSelector);
+
+    // --<button id = "AdvancedModeButton" onmouseover = "hoverToolTipStart(this);" onmouseleave = "hoverToolTipEnd();" onclick = "toggleAdvanced();" style = "background-color: var(--red-button);"> Advanced Mode </button>
+    let advancedModeButton = document.createElement("button");
+    advancedModeButton.id = "AdvancedModeButton";
+    advancedModeButton.onmouseover = function () { hoverToolTipStart(this); };
+    advancedModeButton.onmouseleave = function () { hoverToolTipEnd(); };
+    advancedModeButton.onclick = function () { toggleAdvanced(); };
+    if (advancedMode === true) {
+        advancedModeButton.style.backgroundColor = "var(--green-button)";
+        document.getElementById("AdvancedModeCheckMark").checked = advancedMode;
+    } else {
+        advancedModeButton.style.backgroundColor = "var(--red-button)";
+    }
+    advancedModeButton.textContent = "Advanced Mode";
+    topRightDiv.appendChild(advancedModeButton);
+
+    // have dropdown of gamemodes appear correctly
+    // gameVersionSelector.value = GameVersion;
+
+    // add header to page
+    let scriptTag = document.currentScript;
+    scriptTag.parentNode.replaceChild(topLevel, scriptTag);
 }
